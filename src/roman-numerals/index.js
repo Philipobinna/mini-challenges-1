1b196173dcc7df3e5c0d60f7082fb6724d0a70c5
MAC <mac@MACs-MacBook-Air.local>
2023-08-08 14:42:09 +0100

    i tried my best

--- src/roman-numerals/index.js
+++ src/roman-numerals/index.js
@@ -4,6 +4,28 @@
  * @param {string} roman The all-caps Roman numeral between 1 and 3999 (inclusive).
  * @returns {number} The decimal equivalent.
  */
-function romanToDecimal(roman) {}
+function romanToDecimal(roman) {
+
+let number = {I: 1, V: 5, X: 10, L: 50, C: 100, D: 500, M: 1000};
+
+let lastValue = number[roman[roman.length - 1]];
+let result = lastValue;
+
+for(let i = roman.length-1; i >=0; i--){
+  
+  let nextValue =  number[roman[i - 1]];
+  let currentValue =  number[roman[i]];
+    
+  if(nextValue >= currentValue){
+    result += nextValue;
+  }else if(nextValue < currentValue){
+    result -= nextValue;
+  }
+}
+return result;
+
+}
+console.log(romanToDecimal('MMCDXLVIII'))
 
 module.exports = romanToDecimal;
+
--- src/transpose/index.js
+++ src/transpose/index.js
@@ -4,6 +4,30 @@
  * @param {number[]} array The array to transpose
  * @returns {number[]} The transposed array
  */
-function transpose(array) {}
+function transpose(array) {
+
+  let arr = [
+    [5, 12, 17, 9, 3],
+    [13, 4, 8, 14, 1],
+    [9, 6, 3, 7, 21],
+  ];
+  let transposedArray = [];
+  for (let i = 0; i < originalArray[0].length; i++) {
+    transposedArray.push([]);
+    for (let j = 0; j < originalArray.length; j++) {
+      transposedArray[i].push(originalArray[j][i]);
+    }
+  }
+  
+
+return transposedArray
+
+}
+console.log(transpose([9, 1, 2, 3],
+  [4, 5, 6, 7],
+  [8, 9, 10, 7],
+  [1, 3, 1, 5],
+));
+
 
 module.exports = transpose;
